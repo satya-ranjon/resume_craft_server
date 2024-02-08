5c38cb3f5a0296d0c12f5e6e6d377801b6b051ff
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-08 13:31:24 +0600

    Add TypeScript interfaces for resume data structure
    This commit adds TypeScript interfaces representing the structure of a resume, including personal information, employment history, skills, languages, references, education history, social profiles, section titles, and resume size. These interfaces are designed to be used with Mongoose schemas for storing resume data in a MongoDB database.

---
+++ @types/resume.ts
@@ -0,0 +1,98 @@
+import { Document } from "mongoose";
+
+export interface IResumePersonalInfo extends Document {
+  jobTitle: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  country: string;
+  city: string;
+  address: string;
+  postalCode: string;
+  drivingLicense: string;
+  nationality: string;
+  placeOfBirth: string;
+  DateOfBirth: string;
+}
+
+export interface TypeOfSingleEmploymentHistory extends Document {
+  _id: string | number;
+  city: string;
+  description: string;
+  employer: string;
+  endMontYear: string;
+  jobTitle: string;
+  startMontYear: string;
+}
+
+export interface TypeOfSkill extends Document {
+  _id: string | number;
+  label: string;
+  level: number;
+}
+
+export interface TypeOfLanguage extends Document {
+  _id: string | number;
+  language: string;
+  level: string;
+}
+
+export interface TypeOfReference extends Document {
+  _id: string | number;
+  name: string;
+  company: string;
+  phone: string;
+  email: string;
+}
+
+export interface TypeOfSingleEducationHistory extends Document {
+  _id: string | number;
+  school: string;
+  degree: string;
+  startMontYear: string;
+  endMontYear: string;
+  city: string;
+  description: string;
+}
+
+export interface TypeOfSingleSocialWebSite extends Document {
+  _id: string | number;
+  label: string;
+  link: string;
+}
+
+export interface TypeOfSectionTitle extends Document {
+  personalInfo: string;
+  professionalSummary: string;
+  workExperience: string;
+  skills: string;
+  languages: string;
+  references: string;
+  educations: string;
+  socialProfiles: string;
+}
+
+export interface ISetResumeSize extends Document {
+  height: string;
+  width: string;
+}
+
+export interface IResume extends Document {
+  _id: string | number;
+  templateId: string;
+  avatar: string;
+  personalInfo: IResumePersonalInfo;
+  professionalSummary: string;
+  workExperience: TypeOfSingleEmploymentHistory[];
+  skills: TypeOfSkill[];
+  languages: TypeOfLanguage[];
+  references: TypeOfReference[];
+  educations: TypeOfSingleEducationHistory[];
+  socialProfiles: TypeOfSingleSocialWebSite[];
+  sectionTitles: TypeOfSectionTitle;
+  zoom: number;
+  theme: string;
+  themeOptions: string[];
+  size: ISetResumeSize;
+}
