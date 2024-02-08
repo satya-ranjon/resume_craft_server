b81100cae57a1cfa4a2148b778d26a2166484e85
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-08 13:37:57 +0600

    fix cors issue in env file

--- app/app.ts
+++ app/app.ts
@@ -12,7 +12,7 @@ configDotenv();
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 // router
 app.get("/", (_req: Request, res: Response, _next: NextFunction) => {
