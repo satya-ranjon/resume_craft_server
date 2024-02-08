e009fedfca43425bc756433e6da79869259732b1
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-08 13:31:42 +0600

    Add controller function for creating or updating resumes
    This commit adds a controller function to handle the creation or updating of resume data. The function takes a request containing resume data, attempts to find an existing resume by ID, and either updates it or creates a new one if not found. It returns a success message along with the updated or created resume data in the response. Error handling is implemented to catch and handle any exceptions that may occur during the process.

---
+++ controllers/resume.controller.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { IResume } from "../@types/resume";
+import { catchAsyncError } from "../middlewares/error";
+import ErrorHandler from "../utils/errorHandler";
+import ResumeModel from "../models/resume.modle";
+
+export const createOrUpdateResume = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const resumeData: IResume = req.body;
+
+      const existing = await ResumeModel.findById(resumeData._id);
+
+      const existingResume = await ResumeModel.findOneAndUpdate(
+        { _id: resumeData._id },
+        resumeData,
+        { new: true, upsert: true }
+      );
+
+      res.status(201).json({
+        success: true,
+        message: `Resume ${existing ? "updated" : "created"} successfully.`,
+        resume: existingResume,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
