1ba4f38049d787010350f773710a027d598bbf6d
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-11 02:21:55 +0600

    feat: Add endpoints for managing user history and uploading thumbnails
    This commit introduces three new endpoint handlers for managing user history:
    - : Creates a new history entry based on the request body data.
    - : Retrieves history entries associated with a specific user ID.
    - : Handles the upload of a thumbnail image for a history entry, including deletion of the old thumbnail if it exists.
    
    The  and  handlers utilize the  for database operations, while  utilizes the  middleware for handling file uploads and interacts with Cloudinary services for image uploads and deletion.
    
    Errors are appropriately handled and passed to the error handling middleware for centralized error management.

---
+++ controllers/history.controller.ts
@@ -0,0 +1,97 @@
+import { NextFunction, Request, Response } from "express";
+import uploadPicture from "../middlewares/multerHandle";
+import {
+  deleteImageFromCloudinary,
+  uploadImage,
+} from "../services/cloudinary.services";
+import HistoryModel, { IHistory } from "../models/history.modle";
+import { catchAsyncError } from "../middlewares/error";
+import ErrorHandler from "../utils/errorHandler";
+
+export const createHistory = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data: IHistory = req.body;
+
+      const history = await HistoryModel.create(data);
+      delete history.__v;
+
+      res.status(201).json({
+        success: true,
+        message: `History created successfully.`,
+        history: history,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+export const userHistory = catchAsyncError(
+  async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const history = await HistoryModel.find({
+        user: "65bfd0f85443cc82b0f3f504",
+      });
+
+      res.status(201).json({
+        success: true,
+        history: history,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+export const uploadHistoryThumbnail = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  uploadPicture.single("resumeCraftResumeThumbnail")(req, res, async (err) => {
+    try {
+      if (err) {
+        return next(err);
+      }
+
+      if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+      }
+
+      const { id } = req.params;
+      const history = await HistoryModel.findById(id);
+
+      if (!history) {
+        return res.status(404).json({ message: "History not found" });
+      }
+
+      const oldPublicId = history.thumbnail.public_id;
+      if (oldPublicId) {
+        await deleteImageFromCloudinary(oldPublicId);
+      }
+
+      const avatar = await uploadImage(
+        req.file.buffer,
+        600,
+        860,
+        "resumeCraft_history_thumbnail"
+      );
+
+      history.thumbnail = {
+        url: avatar.secure_url,
+        public_id: avatar.public_id,
+      };
+
+      const updateInfo = await history.save();
+
+      res.status(200).json({
+        success: true,
+        message: "Successfully Upload History Image",
+        thumbnail: updateInfo,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  });
+};
