fc955e27b233f504322886578ce4032bf87b6e4c
Satya Ranjon <satyaranjonofficial@gmail.com>
2024-02-11 02:20:08 +0600

    feat: Add Mongoose model for user history
    This commit introduces a Mongoose model  for storing user history. The model defines the following fields:
    - : Unique identifier for the history entry.
    - : Reference to the user associated with this history entry.
    - : Identifier for the related resume.
    - : Object containing public ID and URL for the thumbnail associated with the history entry.
    - : Type of history entry, which can be one of resume, coverletter, or portfolio.
    
    The model is configured to use timestamps for createdAt and updatedAt fields and excludes the version key.

---
+++ models/history.modle.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IHistory extends Document {
+  _id: string | number;
+  user: Schema.Types.ObjectId;
+  resumeId: string | number;
+  thumbnail: {
+    public_id: string;
+    url: string;
+  };
+  type: "resume" | "coverletter" | "portfolio";
+}
+
+const historySchema = new Schema<IHistory>(
+  {
+    _id: { type: Schema.Types.Mixed, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    resumeId: { type: Schema.Types.Mixed, required: true },
+    thumbnail: {
+      public_id: String,
+      url: String,
+    },
+    type: { type: String, enum: ["resume", "coverletter", "portfolio"] },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const HistoryModel = mongoose.model<IHistory>("History", historySchema);
+
+export default HistoryModel;
